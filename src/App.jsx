82a2566da19b6932730e6b3eda4e199cf1c0d3e1
harsh-bhati <harshvardhanbhati05@gmail.com>
2025-05-18 17:25:52 +0530

    first upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import JobScheduling from './pages/JobScheduling';
 import JobSchedulingSimulator from './pages/JobSchedulingSimulator';
 
 import './styles/scrollbar.css';
@@ -13,16 +8,11 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
-        <Navbar />
         <main className="flex-grow p-4">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/job-scheduling" element={<JobScheduling />} />
             <Route path="/jobSchedulingSimulator" element={<JobSchedulingSimulator />} />
           </Routes>
         </main>
-        <Footer />
       </div>
     </Router>
   );
